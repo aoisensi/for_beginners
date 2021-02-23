b915456fa1dff78c55d8b849aef0797e09edb6ca
aoisensi <me@aoisensi.info>
2021-02-23 11:35:21 +0900

    more remove

--- content/panorama/scripts/custom_game/remove_ui.js
+++ content/panorama/scripts/custom_game/remove_ui.js
@@ -8,4 +8,5 @@ root.FindChildTraverse("GlyphScanContainer").visible = false;
 root.FindChildTraverse("inventory_neutral_slot_container").visible = false;
 root.FindChildrenWithClassTraverse("GridNeutralsTabContainer")[0].visible = false;
 root.FindChildTraverse("AghsStatusContainer").visible = false;
+root.FindChildTraverse("level_stats_frame").visible = false;
 root.FindChildTraverse("StatBranch").visible = false;
